9d5556013b77dd8a70daa05253ae926ded56dc39
SungHeeCheol <107664115+SungHeeCheol@users.noreply.github.com>
2025-01-11 20:37:04 +0900

    first commit

---
+++ src/main/java/com/github/post2501/Post2501Application.java
@@ -0,0 +1,13 @@
+package com.github.post2501;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Post2501Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Post2501Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/post2501/Post2501ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.post2501;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Post2501ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
