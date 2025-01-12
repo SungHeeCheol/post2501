aa1b96143c6d5c951c2bb3e479b02128b55668f7
SungHeeCheol <107664115+SungHeeCheol@users.noreply.github.com>
2025-01-12 23:23:51 +0900

    post ~ing

---
+++ src/main/java/com/github/post2501/post/cotroller/PostController.java
@@ -0,0 +1,12 @@
+package com.github.post2501.post.cotroller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class PostController {
+
+}
---
+++ src/main/java/com/github/post2501/post/repository/PostRepository.java
@@ -0,0 +1,7 @@
+package com.github.post2501.post.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository {
+}
